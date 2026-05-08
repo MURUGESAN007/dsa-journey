fd4ca134e13f6f69e1efacc324c3785c99d0a84c
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-08 09:51:20 +0530

    Time: 5 ms (98.67%) | Memory: 46.7 MB (70.36%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> li = new LinkedList<>();
-        Map<Character,Integer> s2Map = new HashMap<>();
-        Map<Character,Integer> windowMap = new HashMap<>();
-        for(Character c : p.toCharArray()){
-            s2Map.put(c,s2Map.getOrDefault(c,0) + 1);
+        int[] pFreq = new int[26];
+        int[]  sFreq= new int[26];
+
+        for(char c : p.toCharArray()){
+            pFreq[c - 'a']++;
         }
         int left = 0;
         for(int right=0;right<s.length();right++){
-            char rc = s.charAt(right);
-            windowMap.put(rc,windowMap.getOrDefault(rc,0) + 1);
+            sFreq[s.charAt(right) - 'a']++;
             if(right-left+1 > p.length()){
-                char lc = s.charAt(left);
-                windowMap.put(lc,windowMap.get(lc) - 1);
-                if(windowMap.get(lc) == 0){
-                    windowMap.remove(lc);
-                }
+                sFreq[s.charAt(left) - 'a']--;
                 left++;
             }
-            if(windowMap.equals(s2Map)){
+            boolean isMatch = true;
+            for(int i=0;i<26;i++){
+                if(pFreq[i] != sFreq[i]){
+                    isMatch=false;
+                    break;
+                }
+            }
+            if(isMatch){
                 li.add(left);
             }
         }
