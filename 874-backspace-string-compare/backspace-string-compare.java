99ecb35dd8c678e7f682aa6482209b9bf26eb9ef
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-12 16:48:02 +0530

    Time: 2 ms (57.77%) | Memory: 43 MB (51.27%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -4,9 +4,9 @@ class Solution {
     }
     private String check(String s){
         Stack<Character> ans = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) != '#'){
-                ans.push(s.charAt(i));
+        for(char ch : s.toCharArray()){
+            if(ch != '#'){
+                ans.push(ch);
             }else if(!ans.isEmpty()){
                 ans.pop();
             }
