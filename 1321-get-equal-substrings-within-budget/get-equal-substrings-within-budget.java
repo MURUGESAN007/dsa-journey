8d6b60d4806d65dc9a9b28689d70a626faebd8e9
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-12 22:24:16 +0530

    Time: 7 ms (82.06%) | Memory: 44.4 MB (59.46%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int ans = 0,left = 0,window = 0;
+        for(int right=0;right<s.length();right++){
+            window += Math.abs(s.charAt(right) - t.charAt(right));
+            while(window > maxCost){
+                window -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+            ans = Math.max(ans,right-left+1);
+        }
+        return ans;
+    }
+}
