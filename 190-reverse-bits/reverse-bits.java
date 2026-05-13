a62cfc03c97f29f612b1af74bcf23789bce3fc92
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-13 19:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (33.03%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,11 +1,9 @@
-public class Solution {
+class Solution {
     public int reverseBits(int n) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res |= (n & 1);
-            n >>>= 1;
+        int rev = 0;
+        for(int i = 0; i < 32; i++) {
+            rev = (rev << 1) | ((n >> i) & 1);
         }
-        return res;
+        return rev;
     }
 }
