18478f7f4c5eeed3a13d8633f150af0afc8726b6
MURUGESAN007 <128365354+MURUGESAN007@users.noreply.github.com>
2026-05-26 16:54:48 +0530

    Time: 2 ms (48.76%) | Memory: 43.6 MB (81.71%) - LeetSync

--- 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,21 +1,28 @@
 class Solution {
+
     public int numberOfSpecialChars(String word) {
-        
-        HashSet<Character> set = new HashSet<>();
 
-        for(char ch : word.toCharArray()) {
-            set.add(ch);
+        boolean[] lower = new boolean[26];
+        boolean[] upper = new boolean[26];
+        // Traverse string
+        for (char ch : word.toCharArray()) {
+            // lowercase
+            if (Character.isLowerCase(ch)) {
+                lower[ch - 'a'] = true;
+            }
+            // uppercase
+            else {
+                upper[ch - 'A'] = true;
+            }
         }
-
         int count = 0;
+        // Check both exist
+        for (int i = 0; i < 26; i++) {
 
-        for(char ch = 'a'; ch <= 'z'; ch++) {
-
-            if(set.contains(ch) && set.contains((char)(ch - 'a' + 'A'))) {
+            if (lower[i] && upper[i]) {
                 count++;
             }
         }
-
         return count;
     }
 }
